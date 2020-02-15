8b713106b848fc86519f20f639b1d06e6224771c
Solomon Okwa <solozyokwa@gmail.com>
2020-02-15 11:00:42 +0100

    update seed script

--- seed.sql
+++ seed.sql
@@ -1,15 +1,16 @@
 CREATE TABLE users (
   id serial PRIMARY KEY,
   name VARCHAR (50) NOT NULL,
+  email VARCHAR (255) NOT NULL,
   age INT NOT NULL,
   profession VARCHAR (50) NOT NULL,
   friendly BOOLEAN NOT NULL
 );
 
 INSERT INTO users VALUES
-  (1, 'kevin', 35, 'waiter', true),
-  (2, 'angela', 21, 'concierge', true),
-  (3, 'alex', 26, 'zoo keeper', false),
-  (4, 'becky', 67, 'retired', false),
-  (5, 'kevin', 15, 'in school', true),
-  (6, 'frankie', 45, 'teller', true);
+  (1, 'kevin', 'kevin@email.com', 35, 'waiter', true),
+  (2, 'angela', 'angela@email.com', 21, 'concierge', true),
+  (3, 'alex', 'alex@email.com', 26, 'zoo keeper', false),
+  (4, 'becky', 'becky@email.com', 67, 'retired', false),
+  (5, 'kevin', 'kevin2@email.com', 15, 'in school', true),
+  (6, 'frankie', 'frankie@email.com', 45, 'teller', true);
